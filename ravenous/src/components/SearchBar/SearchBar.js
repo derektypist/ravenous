83db253adaeb6e66c030f0a6c91042cb3b8a2456
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-28 09:02:55 +0000

    Inserted contents of arrow function at SearchBar.js

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -9,6 +9,9 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
     renderSortByOptions() {
-        return Object.keys(sortByOptions)
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
+        });
     }
 }
