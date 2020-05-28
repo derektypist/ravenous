9df15e0134fb871d7b7dcd45278de0483ec11d69
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-28 08:52:44 +0000

    Added object at SearchBar.js

---
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'rating_count'
+};
