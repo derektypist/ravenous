37fe7887a1da72dc36fa025ae66f40823bde70a4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 16:22:04 +0000

    Updated Yelp.js

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,10 @@
+const apiKey = "https://gitpod.io/#YELP_API_KEY/https://github.com/derektypist/ravenous";
+
+const yelp = {searchYelp(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+        headers: {
+            Authorization: `Bearer ${apiKey}`
+        }
+    });
+}};
