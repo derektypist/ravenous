5eb3e99dd865182573449cb078d8faa7dd60ee5e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 16:42:02 +0000

    Added export statement to Yelp.js

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -11,8 +11,25 @@ const yelp = {
                 return response.json();
             }).then(jsonResponse => {
                 if (jsonResponse.businesses) {
-                    return jsonResponse.businesses.map();
+                    return jsonResponse.businesses.map(((business) => {
+                        console.log(business);
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+
+                        }
+                    }));
                 }
             })
     }
 };
+
+export default yelp;
