02fe5de7d6c17577e6fcae6bc801df766a70e5b0
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 15:09:46 +0000

    Added onClick for a element at SearchBar.js

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -67,7 +67,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange}/>
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         )
