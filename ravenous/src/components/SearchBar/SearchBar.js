604eb9f135443f10c7dc36f8399d86c8a9ee5bde
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 14:12:56 +0000

    Added className to li element at SearchBar.js

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -33,7 +33,7 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>;
+            return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>;
         });
     }
 
