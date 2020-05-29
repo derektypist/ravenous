72378cbe3fe10a9e04087e275fe859b6d5febd9d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 17:31:58 +0000

    Changed word at Yelp.js2

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -1,7 +1,7 @@
 const apiKey = "https://gitpod.io/#YELP_API_KEY/https://github.com/derektypist/ravenous";
 
 const Yelp = {
-    searchYelp(term, location, sortBy) {
+    search(term, location, sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
             {
                 headers: {
