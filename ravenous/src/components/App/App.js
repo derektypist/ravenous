434178a4d087a34a9e760c87dbcf47a3844f9cbc
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 14:56:35 +0000

    Added searchYelp property at App.js

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
         return (
             <div className="App">
   <h1>ravenous</h1>
-  <SearchBar/>
+  <SearchBar searchYelp={this.searchYelp}/>
   <BusinessList businesses={businesses}/>
 </div>
   );
