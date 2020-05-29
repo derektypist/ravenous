3b540e786442d851243b9b187f440dde53731d98
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 16:31:02 +0000

    Added jsonResponse at Yelp.js

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -1,10 +1,18 @@
 const apiKey = "https://gitpod.io/#YELP_API_KEY/https://github.com/derektypist/ravenous";
 
-const yelp = {searchYelp(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-    {
-        headers: {
-            Authorization: `Bearer ${apiKey}`
-        }
-    });
-}};
+const yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`
+                }
+            }).then(response => {
+                return response.json();
+            }).then(jsonResponse => {
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map();
+                }
+            })
+    }
+};
