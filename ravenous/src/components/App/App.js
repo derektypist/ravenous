82c3ed927432c1b05e49d4f2ca4283b88b90fa4d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 16:57:33 +0000

    Updated searchYelp at App.js

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -3,26 +3,24 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-let business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businesses = [business, business, business, business, business, business];
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            businesses: []
+        };
 
+        this.searchYelp = this.searchYelp.bind(this);
+    }
     searchYelp(term, location, sortBy) {
-        console.log(`You are searching for ${term}, ${location} and ${sortBy}.`);
+        Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+            this.setState({businesses: businesses});
+        });
     }
 
 
@@ -31,7 +29,7 @@ class App extends React.Component {
             <div className="App">
   <h1>ravenous</h1>
   <SearchBar searchYelp={this.searchYelp}/>
-  <BusinessList businesses={businesses}/>
+  <BusinessList businesses={this.state.businesses}/>
 </div>
   );
     }
