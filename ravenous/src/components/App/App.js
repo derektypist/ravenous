8da86302551f15908916979e468ac3014b12ac21
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-29 09:03:37 +0000

    Added business object at App.js

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -4,6 +4,19 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+let business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+
 class App extends React.Component {
     render() {
         return (
